0a3e37f8dcb65bf778b23522ca14e81d7b0b917a
Andre <andre_oliiveira97@hotmail.com>
2022-08-12 00:48:27 -0300

    base project

---
+++ desafio/src/test/java/br/com/gama_academy_ibm/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gama_academy_ibm.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
